59a6d8de1b0667bdbbffc54bfd776ee77ad4affe
hargitoktil <hargitoktil@gmail.com>
2025-10-25 10:58:43 +0700

    sip

--- src/Characters.java
+++ src/Characters.java
@@ -2,10 +2,12 @@ public class Characters {
     public static void main(String[] args) {
         String[] conjugation = {"hide", "hindered", "hiding", "hidden", "hindering", "hid", "hides", "hinder"};
         int lengthConjug = conjugation.length;
+        int numberOfCharackter = 0;
 
         for(int i = 0; i<lengthConjug;i++){
-            System.out.println(conjugation[i]);
+            String word = conjugation[i];
+            numberOfCharackter += word.length();
         }
-    
+    System.out.println("total di array ada "+ numberOfCharackter + " karakter");
     }
 }
