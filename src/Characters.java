8049e6ca57745c082117952d587606f5ced03304
hargitoktil <hargitoktil@gmail.com>
2025-10-25 10:52:08 +0700

    ok

---
+++ src/Characters.java
@@ -0,0 +1,11 @@
+public class Characters {
+    public static void main(String[] args) {
+        String[] conjugation = {"hide", "hindered", "hiding", "hidden", "hindering", "hid", "hides", "hinder"};
+        int lengthConjug = conjugation.length;
+
+        for(int i = 0; i<lengthConjug;i++){
+            System.out.println(conjugation[i]);
+        }
+    
+    }
+}
